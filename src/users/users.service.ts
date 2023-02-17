05b7d5e9430c5c56d1c589df91e1f285073dacbf
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-17 11:20:26 -0300

    criado module, service, controller para usadio

---
+++ src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly service: HttpService) {}
+
+  async auth(body: Body) {
+    const { data } = await this.service.axiosRef.post('https://api.irroba.com.br/v1/getToken',body)
+    return data;
+  }
+}
