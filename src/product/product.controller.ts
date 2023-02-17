72ef753f3e84a35021a94c5a360519c60b62763c
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-17 11:21:23 -0300

    producto for mongodb crud

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,29 +1,71 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, HttpStatus, Res } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { CreateProductDto } from '../dto/create_product.dto';
-import { Product } from 'src/schemas/product.schemas';
+import { CreateProductDto } from './dto/create_product.dto';
+import { Product } from 'src/schemas/product.schemas';import { UpdateProductDto } from './dto/update_product.dto';
+;
 
 @Controller('product')
 export class ProductController {
-    constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateProductDto) {
-    await this.productService.create(createCatDto);
+  async create(@Res() response, @Body() createCatDto: CreateProductDto) {
+    try {
+      const newProduct = await this.productService.create(createCatDto);
+      return response.status(HttpStatus.CREATED).json({
+        message: 'Product has been created sucessfully',
+        newProduct,
+      });
+    } catch(err){
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: 400,
+        message: 'Error: Student not created',
+      });
+    }
+  }
+  @Put('/:id')
+  async update(@Res() response,@Param('id') id: string,
+  @Body() updateProductDto: UpdateProductDto) {
+    try {
+    const existingProduct = await this.productService.update(id, updateProductDto);
+    return response.status(HttpStatus.OK).json({
+    message: 'Product has been successfully updated',
+    existingProduct,});
+  } catch (err) {
+    return response.status(err.status).json(err.response);
+  }
   }
 
   @Get()
-  async findAll(): Promise<Product[]> {
-    return this.productService.findAll();
+  async findAll(@Res() response) {
+    try{
+      const productData = await this.productService.findAll();
+        return response.status(HttpStatus.OK).json({
+        message: 'All product data found successfully',productData,});
+    }catch(err){
+      return response.status(err.status).json(err.response);
+    }
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Product> {
-    return this.productService.findOne(id);
+  async findOne(@Res() response, @Param('id') id: string){
+    try{
+      const existingProduct = await this.productService.findOne(id);
+      return response.status(HttpStatus.OK).json({
+      message: 'Product found successfully',existingProduct,});
+    }catch(err){}
+    return response.status(err.status).json(err.response);
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
-    return this.productService.delete(id);
+  async delete(@Res() response, @Param('id') id: string) {
+    try{
+      const deleteProduct = await this.productService.delete(id);
+      return response.status(HttpStatus.OK).json({
+        message: 'Product deleted successfully',
+        deleteProduct,});
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
   }
 }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Product, ProductSchemas } from 'src/schemas/product.schemas';
+import {  ProductSchemas } from 'src/schemas/product.schemas';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchemas }])],
+    imports: [MongooseModule.forFeature([{ name: 'Product', schema: ProductSchemas }]), HttpModule],
     controllers: [ProductController],
     providers: [ProductService]
 })
