0f6fa5ffbe377e5325dcb0f3012f2b3b30815118
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-17 11:21:07 -0300

    dtos

---
+++ src/product/dto/create_product.dto.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly product_description: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly price: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly sku: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    readonly product_to_category: string;
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,31 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException  } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateProductDto } from 'src/dto/create_product.dto';
+import { CreateProductDto } from './dto/create_product.dto';
 import { Product, ProductDocument } from 'src/schemas/product.schemas';
+import { UpdateProductDto } from './dto/update_product.dto';
 
 
 @Injectable()
 export class ProductService {
     constructor(
-        @InjectModel(Product.name) private readonly productModel: Model<ProductDocument>,
+        @InjectModel('Product') private readonly productModel: Model<ProductDocument>,
     ){}
 
     async create(createProduct:CreateProductDto): Promise<Product>{
         const createdProduct= await this.productModel.create(createProduct);
-        return createdProduct;
+        return createdProduct.save();
+    }
+
+    async update(id: string, updateProductDto: UpdateProductDto ): Promise<Product>{
+        const existingProdut = await this.productModel.findByIdAndUpdate(id, updateProductDto,{
+            new: true
+        });
+        if (!existingProdut) {
+            throw new NotFoundException(`Product #${id} not found`);
+          }
+          return existingProdut;
+
     }
 
     async findAll(): Promise<Product[]> {
-        return this.productModel.find().exec();
+        const productData = await this.productModel.find().exec();
+
+        if (!productData || productData.length == 0) {
+            throw new NotFoundException('Product data not found!');
+        }
+        return productData;
     }
 
     async findOne(id: string): Promise<Product> {
-        return this.productModel.findOne({ _id: id }).exec();
+        const existingProduct =  this.productModel.findOne({ id: id }).exec();
+
+        if (!existingProduct) {
+            throw new NotFoundException(`Product #${id} not found`);
+           }
+           return existingProduct;
     }
     
     async delete(id: string) {
-        const deleteProduct = await this.productModel.findByIdAndRemove({ _id: id }).exec();
+        const deleteProduct = await this.productModel.findByIdAndRemove({ id: id }).exec();
+
+        if (!deleteProduct) {
+            throw new NotFoundException(`Product #${id} not found`);
+          }
+          return deleteProduct;
         return deleteProduct;
     }
 }
