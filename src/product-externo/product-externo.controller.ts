8c79223b2cb3ef24023c71aa06ddef05133de1df
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-17 11:20:54 -0300

    cria produtos e seus modules, services e controler

---
+++ src/product-externo/product-externo.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+import { ProductExternoService } from './product-externo.service';
+
+@Controller('product-externo')
+export class ProductExternoController {
+    constructor(private apiService: ProductExternoService){}
+
+    @Get(':id')
+    async findID(@Param('id') id: string): Promise<any> {
+      const productId = await this.apiService.findId(id);
+      return productId;
+    }
+}
