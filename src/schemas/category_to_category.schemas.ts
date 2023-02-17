98347c78c2d52b16f652490ca3801b6f5edf0f91
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-17 17:11:24 -0300

    adicionando o uuid

--- src/schemas/category_to_category.schemas.ts
+++ src/schemas/category_to_category.schemas.ts
@@ -1,11 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { type } from 'os';
+import {v4 as v4 } from 'uuid';
 
 export type ProductToCategoryDocuments = HydratedDocument<ProductToCategory>;
 
 @Schema()
 export class  ProductToCategory{
+    @Prop()
+    id: v4;
+
     @Prop()
     product_to_category: string;
 
