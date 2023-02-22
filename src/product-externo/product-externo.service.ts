d3f6c6baec5fa9dabff10ec3a5fa9086b4a9df63
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-22 08:24:54 -0300

    mudei findid

--- src/product-externo/product-externo.service.ts
+++ src/product-externo/product-externo.service.ts
@@ -3,12 +3,25 @@ import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class ProductExternoService {
-    constructor(private readonly service: HttpService){}
+  constructor(private readonly service: HttpService){}
 
-    async findId(id: string){
-        const { data } = await this.service.axiosRef.get(`https://api.irroba.com.br/v1/product/${id}`);
-        console.log(data)
-        return data
-    }
+  async findId(id: string) {
+    const { data } = await this.service.axiosRef.get(`
+    https://api.irroba.com.br/v1/product/${id}`);
+    return data;
+  }
 
+//   async findProduct(id: string) {
+//     const { data } = await this.service.axiosRef.get(`
+//     https://api.irroba.com.br/v1/product/${id}`,
+//       {
+//         headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': 'Bearer '+ token,
+//         }
+//       });
+//     console.log(data);
+//     console.log(data.authorization);
+//     return data;
+//   }
 }
